f6063f95472dfec0d3253ede0d51161ed3353e8a
TigranSid <34732633+TigranSid@users.noreply.github.com>
2017-12-21 22:39:33 -0500

    Ajour URLs

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
 from flask import Flask
-		
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Hello, Worldcenzo!'
-if __name__ == '__main__':
-	app.run(debug=True)
+   return "Ceci est la page d'accueil."
+   
+@app.route('/hello/<phrase>')
+def hello(phrase):
+   return phrase
